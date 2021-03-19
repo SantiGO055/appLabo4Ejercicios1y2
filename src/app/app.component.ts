d99bae52b6a976f8a7e9f08515d2fb3ac96b7407
SantiGO055 <santigonzalez05@gmail.com>
2021-03-18 22:34:50 -0300

    Primer commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'appejercicios1y2';
+}
