8852fce5a45a2f0914276081c6da42120572d836
SantiGO055 <santigonzalez05@gmail.com>
2021-03-19 20:27:27 -0300

    Agrego ruteos simples

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { LoginComponent } from './login/login.component';
+import { BienvenidoComponent } from './bienvenido/bienvenido.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'BienvenidoComponent',component: BienvenidoComponent },
+  { path: 'LoginComponent', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
