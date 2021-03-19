ebb26c743b84825a34db1005996fa46b2b2a9b59
SantiGO055 <santigonzalez05@gmail.com>
2021-03-18 22:55:11 -0300

    Subo ejercicio 1 terminado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appejercicios1y2';
+  title = 'Ejercicio1';
+  constructor(){}
+  edad1:number = 0;
+  edad2:number = 0;
+  resultadoSuma:number= 0;
+  resultadoPromedio:number= 0;
+  ocultar = true;
+  
+  CalcularSuma(){
+    
+    this.resultadoSuma = this.edad1 + this.edad2;
+  }
+  CalcularPromedio(){
+    
+    this.resultadoPromedio = (this.edad1 + this.edad2) / 2;
+  }
+  MostrarResultado(){
+    this.ocultar = true;
+  }
+  LimpiarTexto(){
+    this.resultadoPromedio = 0;
+    this.resultadoSuma = 0;
+    this.edad1 = 0;
+    this.edad2 = 0;
+    
+  }
 }
